571a6ef8ed21b30cb500522271b1118fd0bca057
Ronnie S <58877182+rsewankambo@users.noreply.github.com>
2020-09-14 02:05:18 -0700

    Starter code

---
+++ src/main/java/com/udacity/jwdnd/course1/superdrive/SuperDriveApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.course1.superdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDriveApplication.class, args);
+	}
+	
+}
---
+++ src/main/java/com/udacity/jwdnd/course1/superdrive/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.udacity.jwdnd.course1.superdrive.config;
+
+import com.udacity.jwdnd.course1.superdrive.service.AuthenticationService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    private AuthenticationService authenticationService;
+    
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(this.authenticationService);
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/home", true)
+                .and()
+                .logout().permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+}
